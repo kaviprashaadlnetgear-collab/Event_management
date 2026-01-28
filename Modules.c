ab71450640d8bb44d674683390673796d7d861f0
kaviprashaadl <kaviprashaadlnetgear@gmail.com>
2026-01-28 14:51:22 +0530

    Create Modules.c

---
+++ Modules.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void Notification(){
+    printf("\n ---------------------Notificatio Module triggered---------------------\n");
+}
+void P(){
+     printf("\n----------------------P Module triggered----------------------\n");
+
+}
+void Q(){
+     printf("\n----------------------Q Module triggered-------------------------\n");
+}
+void Z(){
+     printf("------------------------Z Module triggered-------------------------\n");
+}
+
+void Y(){
+     printf("------------------------Y Module triggered--------------------------\n");
+}
